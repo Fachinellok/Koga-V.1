02f02fdfda3c772a95decc8bb59674cba30f96ed
Fachinello <95387626+Fachinellok@users.noreply.github.com>
2023-05-24 23:34:47 -0300

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -9,62 +9,62 @@ module.exports = function (client) {
         filename: "welcome.js",
         version: "4.8"
     }
-    //log that the module is loaded
+    //log que o módulo está carregado
     console.log(` :: ⬜️ Module: ${description.name} | Loaded version ${description.version} from ("${description.filename}")`)
-    //fires every time when someone joins the server
+    //log que o módulo está carregado
     client.on("guildMemberAdd", async member => {
-      //If not in a guild return
+      //log que o módulo está carregado
       if(!member.guild) return;
-      //create a new Canvas
+      //criar uma nova tela
       const canvas = Canvas.createCanvas(1772, 633);
-      //make it "2D"
+      //torná-lo '2D'
       const ctx = canvas.getContext('2d');
-      //set the Background to the welcome.png
+      //defina o plano de fundo como welcome.png
       const background = await Canvas.loadImage(`./welcome.png`);
       ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
       ctx.strokeStyle = '#f2f2f2';
       ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      //set the first text string 
+      //definir a primeira string de texto
       var textString3 = `${member.user.username}`;
-      //if the text is too big then smaller the text
+      //se o texto for muito grande então menor o texto
       if (textString3.length >= 14) {
         ctx.font = 'bold 100px Genta';
         ctx.fillStyle = '#f2f2f2';
         ctx.fillText(textString3, 720, canvas.height / 2 + 20);
       }
-      //else dont do it
+      //senão não faça isso
       else {
         ctx.font = 'bold 150px Genta';
         ctx.fillStyle = '#f2f2f2';
         ctx.fillText(textString3, 720, canvas.height / 2 + 20);
       }
-      //define the Discriminator Tag
+      //definir a Discriminator Tag
       var textString2 = `#${member.user.discriminator}`;
       ctx.font = 'bold 40px Genta';
       ctx.fillStyle = '#f2f2f2';
       ctx.fillText(textString2, 730, canvas.height / 2 + 58);
-      //define the Member count
+      //definir o Member count
       var textString4 = `Member #${member.guild.memberCount}`;
       ctx.font = 'bold 60px Genta';
       ctx.fillStyle = '#f2f2f2';
       ctx.fillText(textString4, 750, canvas.height / 2 + 125);
-      //get the Guild Name
+      //pega a Guild Name
       var textString4 = `${member.guild.name}`;
       ctx.font = 'bold 60px Genta';
       ctx.fillStyle = '#f2f2f2';
       ctx.fillText(textString4, 700, canvas.height / 2 - 150);
-      //create a circular "mask"
+      //cria uma máscara "mask" circular
       ctx.beginPath();
-      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
+      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//senão não faça isso
       ctx.closePath();
       ctx.clip();
-      //define the user avatar
+      //define o user avatar
       const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
       //draw the avatar
       ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
-      //get it as a discord attachment
+      //obter o anexo do discord
       const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-      //define the welcome embed
+      //ddefinir o welcome embed
       const welcomeembed = new Discord.MessageEmbed()
         .setColor("RANDOM")
         .setTimestamp()
@@ -73,11 +73,11 @@ module.exports = function (client) {
       Hi <@${member.id}>!, read and accept the rules!`)
         .setImage("attachment://welcome-image.png")
         .attachFiles(attachment);
-      //define the welcome channel
+      //define o welcome channel
       const channel = member.guild.channels.cache.find(ch => ch.id === config.CHANNEL_WELCOME);
-      //send the welcome embed to there
+      //manda a welcome embed para o canal
       channel.send(welcomeembed);
-      //member roles add on welcome every single role
+      //os cargos dos membros adicionam boas vindas a cada um
       let roles = config.ROLES_WELCOME;
       for(let i = 0; i < roles.length; i++ )
       member.roles.add(roles[i]);
